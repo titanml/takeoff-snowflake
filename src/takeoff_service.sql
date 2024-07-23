22756129c920e714e610eeb9ceb916522de489d5
yc-wang <yicheng.wang.2023@gmail.com>
2024-07-23 15:40:49 +0100

    chore: modify takeoff_service.sql

--- src/takeoff_service.sql
+++ src/takeoff_service.sql
@@ -2,11 +2,21 @@
 
 // Create the takeoff service
 USE ROLE CONTAINER_USER_ROLE;
+
+
+// **Note: use the following command if you work with normal takeoff in snowflake demo
+-- create service CONTAINER_HOL_DB.PUBLIC.takeoff_service
+--     in compute pool CONTAINER_HOL_POOL
+--     from @specs
+--     specification_file='takeoff-snowflake.yaml'
+--     external_access_integrations = (ALLOW_ALL_EAI);
+
+// **Note: use the following command if you work with Dataiku plugin demo
 create service CONTAINER_HOL_DB.PUBLIC.takeoff_service
     in compute pool CONTAINER_HOL_POOL
     from @specs
     specification_file='takeoff-snowflake.yaml'
-    external_access_integrations = (ALLOW_ALL_EAI);
+    external_access_integrations = (SNOWFLAKE_EGRESS_ACCESS_INTEGRATION); 
 
 
 // Test takeoff service: get service status
@@ -19,3 +29,4 @@ CALL SYSTEM$GET_SERVICE_LOGS('CONTAINER_HOL_DB.PUBLIC.TAKEOFF_SERVICE', '0', 'ta
 
 // Show endpoints in service
 SHOW ENDPOINTS IN SERVICE TAKEOFF_SERVICE;
+
